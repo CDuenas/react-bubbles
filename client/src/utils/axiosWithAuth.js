063c3362c6293c07b0a70257ecc56a9af8b694d0
Christian <Chrisaduenas@gmail.com>
2020-03-20 16:32:53 -0700

    GET request successfully pulling data from server

--- client/src/utils/axiosWithAuth.js
+++ client/src/utils/axiosWithAuth.js
@@ -6,7 +6,9 @@ export const axiosWithAuth = () => {
     return axios.create({
     baseURL: "http://localhost:5000/api",
     headers: {
-        Authorization: token
-    }
-});
+            Authorization: token
+        }
+    });
 };
+
+export default axiosWithAuth;
